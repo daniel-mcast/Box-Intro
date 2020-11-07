2dea0f246861e547e74bc5a210cdecc6465be4ed
danzaq <cassardan0@gmail.com>
2020-11-07 17:26:49 +0100

    Created the first few steps of the box exercise.

---
+++ Introduction Exercies/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //the player moves with this speed.
+    private int _speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Point 1 Use to explain the Start method
+        //Pseudo 1. The player position is reset to 0 on all three axis at the start of the game.
+        this.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Pseudo 2. The player moves up on the y axis.
+        this.transform.Translate(0, _speed * Time.deltatime, 0);
+    }
+}
