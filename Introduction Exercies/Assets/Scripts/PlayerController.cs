774320227d96df31b4b60bee4d30126624ef014c
danzaq <cassardan0@gmail.com>
2020-11-07 19:03:36 +0100

    Finished box exercise
    Added colour changing as box changes direction.
    Added also box changing direction.
    modified box from moving up to left and right.

--- Introduction Exercies/Assets/Scripts/PlayerController.cs
+++ Introduction Exercies/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //the player moves with this speed.
-    private int _speed = 2;
+    //the speed at which the player moves at.
+    public int speed = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +18,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Pseudo 2. The player moves up on the y axis.
-        this.transform.Translate(0, _speed * Time.deltatime, 0);
+
+        //Point 3 Introduce the concept of if statements with Inputs using arrow keys.
+        //Pseudo 3 When the right arrow is pressed the player moves to the right.
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Point 2 Show the difference between speed with Time and without
+            //Pseudo 2. The player moves right on the x axis.(N.B the player moves on the positive values)
+            this.transform.Translate(speed * Time.deltaTime,0,0);
+            //Point 6 Point out step by step what the code is doing.(Show link for unity answers https://answers.unity.com/questions/984554/change-color-of-a-gameobject-when-it-collisions-c.html)
+            //Pseudo 6 The player colour is changed to blue when it moves to the right.
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        //Point 4 Introduce the concept of multiple if statements in the same script and explain the difference between if/elseif and else
+        //Pseudo 4 When the left arrow is pressed the player moves to the left.
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Point 5 Point out that because we want the player to move to the left we use the negative of the variable speed.
+            //Pseudo 5 The player moves left on the x axis.(N.B the player moves on the negative values)
+            this.transform.Translate(-speed * Time.deltaTime,0,0);
+            //Point 7 Point out step by step what the code is doing.
+            //Pseudo 7 The player colour is changed to blue when it moves to the right.
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+        
+        
+
     }
 }
